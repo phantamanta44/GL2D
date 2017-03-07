39e247932c9c934ba2734c9b2dc6d5373003ee0e
phantamanta44 <evandalong@gmail.com>
2017-03-07 17:55:20 -0600

    Fix casting issue and document event bus

--- src/main/java/io/github/phantamanta44/shlgl/event/EventBus.java
+++ src/main/java/io/github/phantamanta44/shlgl/event/EventBus.java
@@ -7,23 +7,38 @@ import java.util.Map;
  * Handles the dispatching of events to individual {@link EventStream}s.
  * @author Evan Geng
  */
-public class EventBus { // TODO Document
+public class EventBus {
 
-    private final Map<Class<? extends Event>, EventStream<?>> eventStreams;
+    /**
+     * The mapping of event types to {@link EventStream}s.
+     */
+    private final Map<Class<? extends Event>, EventStream> eventStreams;
 
+    /**
+     * Creates and initializes the event bus.
+     */
     public EventBus() {
         eventStreams = new HashMap<>();
         // TODO Register default event types
     }
 
+    /**
+     * Registers an event type.
+     * @param eventType The new event type.
+     */
     public void registerEvent(Class<? extends Event> eventType) {
         eventStreams.put(eventType, new EventStream<>());
     }
 
+    /**
+     * Dispatches an event to all eligible listeners.
+     * @param event The event to post.
+     */
+    @SuppressWarnings("unchecked")
     public void post(Event event) {
         eventStreams.entrySet().stream()
                 .filter(e -> e.getKey().isAssignableFrom(event.getClass()))
-                .forEach(e -> e.getValue().post(event)); // FIXME Cast this somehow
+                .forEach(e -> e.getValue().post(event));
     }
 
 }
